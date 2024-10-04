4d98385380af3510c6b3b64e75a923b74ac10aa4
Terrance Myles Jr <123514008+TeMyls@users.noreply.github.com>
2024-10-04 13:33:36 -0500

    Add files via upload

--- Tkinter File Manipulation/main.py
+++ Tkinter File Manipulation/main.py
@@ -14,7 +14,6 @@ from tkinter import ttk, filedialog
 
 
 
-
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -55,11 +54,14 @@ class PathForm(ttk.Frame):
         
         self.selected_file_listbox = tk.Listbox(self, 
                                                 yscrollcommand = self.selected_file_scrollbar_y.set,
-                                                xscrollcommand = self.selected_file_scrollbar_x.set
+                                                xscrollcommand = self.selected_file_scrollbar_x.set,
+                                                selectmode="multiple", 
+                                                exportselection=False
                                                 )
         
         self.selected_file_scrollbar_x.config(command=self.selected_file_listbox.xview)
         self.selected_file_scrollbar_y.config(command=self.selected_file_listbox.yview)
+        self.selected_file_clear_button = tk.Button(self, text="Clear Files", command=self.clear_files)
         #Batch or By File
         
         #Conversion Button
@@ -67,18 +69,14 @@ class PathForm(ttk.Frame):
         
         self.convert_to_combobox = ttk.Combobox(self, width = 15,  textvariable = tk.StringVar()) 
         
-        self.convert_to_combobox["values"] = (  ""
+        self.convert_to_combobox["values"] = (  "No Change"
                                                 "mp4",
                                                 "ogv",
                                                 "webm",
                                                 "gif",
-                                                "image sequence"
                                               )
         
-        
-        
-        
-        
+
         #Mute
         self.audio_label = ttk.Label(self, text="Audio")
         self.muted_label = ttk.Label(self, text="muted")
@@ -87,12 +85,6 @@ class PathForm(ttk.Frame):
                                                 offvalue = 1
                                                 
                                                 ) 
-        
-        
-        
-       
-        
-        
         
         #Resize
         self.resize_label = ttk.Label(self, text = "Resize")
@@ -134,7 +126,9 @@ class PathForm(ttk.Frame):
                                             )
         
         self.save_path_scrollbar_x.config(command=self.save_path_listbox.xview)
-        self.save_path_button = tk.Button(self, text="Save File(s)", command=self.save_files)
+        self.save_path_button = tk.Button(self, text="Select Folder", command=self.get_directory)
+        self.save_save = tk.Button(self, text="save", command=self.save_files)
+        
         
         
         
@@ -156,6 +150,10 @@ class PathForm(ttk.Frame):
         
         self.combine_listbox.bind("<<ListboxSelect>>", lambda _: self.get_selected_videos(self.combine_listbox))
         
+        #Crop
+        self.crop_label = ttk.Label(self, text="Crop Video")
+        self.crop_video_img_canavs = tk.Canvas(self)
+        
         
         #grid positions
 
@@ -164,9 +162,12 @@ class PathForm(ttk.Frame):
             [self.selected_file_label      , None                          , self.convert_to_label   , self.combine_label     , None                     , self.resize_label    , self.save_path_label        ],
             [self.selected_file_listbox    , self.selected_file_scrollbar_y, self.convert_to_combobox, self.combine_listbox   , self.combine_scrollbar_y ,  self.resize_spinbox , self.save_path_listbox      ],
             [self.selected_file_scrollbar_x, None                          , None                    ,self.combine_scrollbar_x, None                     , None                 , self.save_path_scrollbar_x  ],
-            [self.selected_file_open_button, None                          , self.cut_label          , None                   , self.audio_label         , None                 , self.save_path_button       ],
-            [None                          , self.start_cut_label          , self.cut_start_spinbox  , None                   , self.mute_checkbutton    , self.muted_label     , self.save_name_label        ],                     
-            [None                          , self.end_cut_label            , self.cut_end_spinbox    , None                   , None                     , None                 , self.save_name_entry        ]                           
+            [self.selected_file_open_button, None                          , None                    , None                   , None                     , None                 , self.save_path_button       ],
+            [self.selected_file_clear_button,None                          , self.cut_label          , None                   , self.audio_label         , None                 , self.save_name_label        ],                     
+            [None                          , self.start_cut_label          , self.cut_start_spinbox  , None                   , self.mute_checkbutton    , self.muted_label     , self.save_name_entry        ],    
+            [None                          , self.end_cut_label            , self.cut_end_spinbox    , None                   , None                     , None                 , None                        ],  
+            [None                          , None                          , None                    , None                   , None                     , None                 , None                        ], 
+            [None                          , None                          , None                    , None                   , None                     , None                 , None                        ]                       
             
             
         ]
@@ -182,7 +183,7 @@ class PathForm(ttk.Frame):
                     if isinstance(arrangement[ind_y][ind_x], tk.Label):
                         arrangement[ind_y][ind_x].grid(row = ind_y, column = ind_x, sticky = "E")
                     elif isinstance(arrangement[ind_y][ind_x], tk.Scrollbar):
-                        arrangement[ind_y][ind_x].grid(row = ind_y, column = ind_x, sticky = "NWS")
+                        arrangement[ind_y][ind_x].grid(row = ind_y, column = ind_x, sticky = "NEWS")
                     elif isinstance(arrangement[ind_y][ind_x], tk.Spinbox):
                         arrangement[ind_y][ind_x].grid(row = ind_y, column = ind_x, sticky = "N", columnspan = 1)
                     elif isinstance(arrangement[ind_y][ind_x], tk.Button):
@@ -220,12 +221,12 @@ class PathForm(ttk.Frame):
         
         if file_path_list:
             #text = "\n".join(file_path_list)
-            for ind in range(self.selected_file_listbox.size()):
-                self.selected_file_listbox.delete(ind)
-            
             for ind in range(len(file_path_list)):
-                edited_file = file_path_list[ind].split('/')[-1]
-                self.selected_file_listbox.insert(ind, edited_file)
+               raw_file = file_path_list[ind]
+               edited_file = raw_file.split('/')[-1]
+               
+               
+            self.selected_file_listbox.insert(0, *file_path_list)
             
         
             temp = file_path_list[0].split('/')
@@ -233,17 +234,29 @@ class PathForm(ttk.Frame):
             
             self.process_file(file_path_list)
             
-    def save_files(self):
+    def clear_files(self):
+        if self.selected_file_listbox.size() > 0:
+            self.selected_file_listbox.delete(0, tk.END)
+        
+            
+    def get_directory(self):
         current_directory = filedialog.askdirectory(
             initialdir = self.last_folder
         )
         file_name = os.path.join(self.last_folder, self.save_name_entry.get())       
+        self.save_path_listbox.insert(tk.END, self.last_folder)
+        
         print(self.last_folder)
-        print(file_name)           
+        print(file_name)      
+        
+    def save_files(self):
+        VideoFileClip()
 
     def get_selected_videos(self, lb):
        # Get selected values from the Listbox widget
         selected_videopaths = [lb.get(idx) for idx in lb.curselection()]
+        for i in selected_videopaths:
+            print(i)
         
 
     def process_file(self, file_path_list):
